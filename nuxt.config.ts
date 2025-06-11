adc08f8cdcf5062bd96b828e2b3a199ae27cbc79
Yoki Jati Perkasa <yokijati@gmail.com>
2025-06-11 23:40:26 +0700

    Init

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+import tailwindcss from "@tailwindcss/vite";
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2025-05-15",
+  devtools: { enabled: true },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  css: ["~/assets/css/main.css"],
+  modules: ["@nuxt/image", "@nuxt/icon", "@nuxt/fonts"],
+});
